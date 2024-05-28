c70c014e1ab25eeaf2f30dbebac8b97e03c2f9b8
abhinav-pandey29 <58676009+abhinav-pandey29@users.noreply.github.com>
2024-05-28 14:52:00 +1000

    Add script to scrape product categories

---
+++ scrape_categories.py
@@ -0,0 +1,34 @@
+"""
+Script to scrape all available categories on the Coles website.
+"""
+
+import os
+import json
+
+from utils.webdriver import initialize_driver
+from pages.categories import CategoriesPage
+
+DEST_DIR = "./data/raw"
+
+
+def dump_categories(categories):
+    os.makedirs(DEST_DIR, exist_ok=True)
+    categories_file = os.path.join(DEST_DIR, "product-categories.json")
+    with open(categories_file, "w") as dst:
+        json.dump(categories, dst)
+
+
+if __name__ == "__main__":
+
+    driver, service = initialize_driver()
+    driver.implicitly_wait(15)
+
+    page = CategoriesPage(driver=driver)
+    page.open()
+
+    found_categories = page.list_categories()
+    if found_categories:
+        dump_categories(found_categories)
+
+    driver.quit()
+    service.stop()
